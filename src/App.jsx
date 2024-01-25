2967bc21988aac9d49c8f43987e22ffc9d349fee
HuaHuaChiChi <ksksks102@naver.com>
2024-01-25 20:36:56 +0900

    feat : 컴포넌트 제작

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/layout/Header";
+import TodoController from "./components/todo/TodoController";
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <TodoController/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/todo/TodoController.jsx
@@ -0,0 +1,8 @@
+
+function TodoController() {
+  return (
+    <div>TodoController</div>
+  )
+}
+
+export default TodoController
