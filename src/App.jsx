c30b0d9958dacb000f69d63337f313c4623fde70
HuaHuaChiChi <ksksks102@naver.com>
2024-01-26 15:19:02 +0900

    "feat : Footer"

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/layout/Header";
 import TodoController from "./components/todo/TodoController";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
     <div>
       <Header/>
       <TodoController/>
+      <Footer></Footer>
     </div>
   );
 }
